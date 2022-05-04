3512278f5ae3c9d0cd1f6a0fbf1559733d05ce91
sonic-san <tevin.abhay@gmail.com>
2022-05-04 12:29:35 +0200

    Date fixé à aujourd'hui

--- tpGestionSavoirs/savoir.js
+++ tpGestionSavoirs/savoir.js
@@ -3,3 +3,18 @@
 focusMethod = function getFocus() {
     document.getElementById("savoirFocus").focus();
 }
+
+/* à regarder pour récupérer la valeur de la date en JS
+https://developer.mozilla.org/fr/docs/Web/HTML/Element/input/date */
+
+//c/c de https://www.demo2s.com/javascript/javascript-date-set-input-type-date-s-default-value-to-today.html //
+
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+var today = year + "-" + month + "-" + day;
+document.getElementById('theDate').value = today;
+
