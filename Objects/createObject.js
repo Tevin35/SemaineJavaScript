342c9b549c58fe480618ea87870e88cec907bfe5
sonic-san <tevin.abhay@gmail.com>
2022-05-04 09:58:01 +0200

    Afficher infos de sport

--- Objects/createObject.js
+++ Objects/createObject.js
@@ -1,6 +1,23 @@
 
 
-let sport = {nom : "Curling", description : "Pétanque sur glace", nbJoueurs : 3}
+let sport = {
+    nom : "Curling",
+    description : "Pétanque sur glace",
+    nbJoueurs : 3
+}
 console.log(sport);
 console.log(sport.nom);
 console.log(sport['description']);
+sport.pays="Jamaïque"
+console.log(sport);
+
+let sport2 = {
+    nom : "Badminton",
+    description : "Jeu de raquettes",
+    nbJoueurs : 2,
+    afficher: function() {
+    console.log("Sport = " + this.nom + ", description = " + this.description)
+    }
+}
+
+sport2.afficher()
